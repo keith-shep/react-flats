f444d235b3493dcb3054f735ed868bd3720cbc89
Keith Shep <kmson7@gmail.com>
2021-01-11 00:33:41 +0800

    couldnt get dotenv to work

--- src/components/GoogleMapReact.jsx
+++ src/components/GoogleMapReact.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-// require('dotenv').config();
-
 const GoogleMapReact = () => {
   return (
     <div className="map-container">
       hello from map
-      {console.log('hello')}
     </div>
   );
 };
